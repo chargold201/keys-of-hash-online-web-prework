dfc91894c58bde2b87547541888c870696fef6cf
Charlotte Goldthwaite <chargold201@gmail.com>
2019-03-25 22:00:28 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,10 +2,9 @@ require 'pry'
 
 class Hash
   def keys_of(*arguments)
-    argument_array = [*arguments]
     array = []
     self.each do |k, v|
-      if argument_array.include? v
+      if *arguments == v
         array << k
       end
     end
