82d703d04dfa79a6efd0dbf7bbef24dd34d35d3e
Charlotte Goldthwaite <chargold201@gmail.com>
2019-03-25 21:58:19 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,9 +2,10 @@ require 'pry'
 
 class Hash
   def keys_of(*arguments)
+    argument_array = [arguments]
     array = []
     self.each do |k, v|
-      if v == arguments
+      if argument_array.include? v
         array << k
       end
     end
